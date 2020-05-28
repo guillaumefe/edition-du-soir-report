ecb8a8500f3dac6914a40eeed90e2ee766bb616a
Guillaume <guillaume.ferron@gmail.com>
2020-05-28 15:13:15 +0200

    increase api detail

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 data = {}
 
 input = {
-    "sum": 'data/summary.csv',
+    "data": 'data/summary.csv',
     "trend": 'data/trend.csv'
 }
 
 methods = ['POST', 'GET']
 
+roots = ['date', 'casConfirmes', 'hospitalises', 'gueris', 'reanimation', 'deces', 'casEhpad', 'casConfirmesEhpad', 'decesEhpad']
 for type in input:
     data[type] = {}
     with open(input[type]) as document:
@@ -20,10 +21,18 @@ for type in input:
         for row in reader:
             dt = datetime.strptime(row[0], '%Y-%m-%d')
             index = dt.strftime('%Y%m%d')
-            bloc = []
+            category = {}
+            # bloc = []
+            last = None
             for key, value in enumerate(row):
-                bloc.append({field[key]:(value or None)})
-            data[type][index] = bloc
+                if field[key] in roots:
+                    last = field[key]
+                    category[last] = {}
+                    category[last]['current'] = (value or None)
+                else:
+                    category[last][field[key]] = (value or None)
+                # bloc.append({field[key]:(value or None)})
+            data[type][index] = category
 
 
 # Catalog
@@ -35,6 +44,18 @@ def map_site():
     output += ''.join([f'/<b>{x}/last</b> {y}<br/>' for x in data for y in methods])
     output += '---<br/>'
     output += ''.join(['/<b>{}/[date]</b> {}<br/>'.format(x, y) for x in data for y in methods])
+    output += '---<br/>'
+    output += ''.join(['/<b>{}/[date]/[category]</b> {}<br/>'.format(x, y) for x in data for y in methods])
+    output += '---<br/>'
+    output += ''.join(['/<b>{}/[date]/[category]/[subcategory]</b> {}<br/>'.format(x, y) for x in data for y in methods])
+    output += '---<br/>'
+    for type in data:
+        for date in sorted(data[type], reverse=True):
+            for category in data[type][date]:
+                for method in methods:
+                    output += '/<b>{}/{}/{}</b> {}<br/>'.format(type, date, category, method)
+                    for subcat in data[type][date][category]:
+                        output += '/<b>{}/{}/{}/{}</b> {}<br/>'.format(type, date, category, subcat, method)
     return output
 
 @app.route('/<type>', methods=methods)
@@ -49,6 +70,14 @@ def last_one(type):
 @app.route('/<type>/<date>', methods=methods)
 def by_date(type, date):
     return json.dumps(data[type][date], ensure_ascii=True)
+
+@app.route('/<type>/<date>/<category>', methods=methods)
+def by_cat(type, date, category):
+    return json.dumps(data[type][date][category], ensure_ascii=True)
+
+@app.route('/<type>/<date>/<category>/<subcategory>', methods=methods)
+def by_subcat(type, date, category, subcategory):
+    return json.dumps(data[type][date][category][subcategory], ensure_ascii=True)
 
 # Serve basic view from templates folder
 @app.route('/', methods=['GET'])
